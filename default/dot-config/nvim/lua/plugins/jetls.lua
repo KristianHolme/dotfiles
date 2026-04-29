42f8718989f4e3cba8365d2c312ad4cc6581d738
Kristian Holme <holme.kristian@gmail.com>
2026-04-29 16:47:14 +0200

    update bindings and jetls config

--- default/dot-config/nvim/lua/plugins/jetls.lua
+++ default/dot-config/nvim/lua/plugins/jetls.lua
@@ -7,11 +7,38 @@ return {
         julials = {
           enabled = false,
         },
-        -- Configure JETLS instead
+        -- JETLS: reference code lens needs editor.action.showReferences (2026-04-28+)
         jetls = {
           cmd = { "jetls", "serve" },
           filetypes = { "julia" },
           root_markers = { "Project.toml", ".git" },
+          settings = {
+            jetls = {
+              code_lens = {
+                references = true,
+                testrunner = true,
+              },
+            },
+          },
+          commands = {
+            ["editor.action.showReferences"] = function(command, ctx)
+              local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
+              local file_uri, position, references = unpack(command.arguments)
+              local items = vim.lsp.util.locations_to_items(
+                references,
+                client.offset_encoding
+              )
+              vim.fn.setqflist({}, " ", {
+                title = command.title,
+                items = items,
+              })
+              vim.lsp.util.show_document({
+                uri = file_uri,
+                range = { start = position, ["end"] = position },
+              }, client.offset_encoding)
+              vim.cmd("botright copen")
+            end,
+          },
         },
       },
     },
